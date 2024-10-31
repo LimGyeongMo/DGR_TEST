af00579e0358b0520c6c0a7608472769af077832
LimGyeongMo <133728406+LimGyeongMo@users.noreply.github.com>
2024-10-31 17:27:47 +0900

    2024.10.31 - test13 (R클레스 최소화)

---
+++ dgr/src/main/java/com/project/dgr/getColor.kt
@@ -0,0 +1,191 @@
+package com.project.dgr
+
+object getColor {
+    val transparent = R.color.transparent
+
+    // White
+    val white = R.color.white
+    val white_40 = R.color.white_40
+    val white_50 = R.color.white_50
+    val white_60 = R.color.white_60
+    val white_70 = R.color.white_70
+    val white_80 = R.color.white_80
+    val white_90 = R.color.white_90
+    val white_95 = R.color.white_95
+
+    // Black
+    val black = R.color.black
+    val black_15 = R.color.black_15
+    val black_25 = R.color.black_25
+    val black_35 = R.color.black_35
+    val black_50 = R.color.black_50
+    val black_60 = R.color.black_60
+    val black_90 = R.color.black_90
+
+    // Grey
+    val grey900 = R.color.grey900
+    val grey900_30 = R.color.grey900_30
+
+    val grey800 = R.color.grey800
+    val grey800_20 = R.color.grey800_20
+    val grey800_30 = R.color.grey800_30
+    val grey800_50 = R.color.grey800_50
+    val grey800_60 = R.color.grey800_60
+    val grey800_70 = R.color.grey800_70
+    val grey800_80 = R.color.grey800_80
+    val grey800_90 = R.color.grey800_90
+
+    val grey700 = R.color.grey700
+    val grey700_40 = R.color.grey700_40
+
+    val grey600 = R.color.grey600
+    val grey600_40 = R.color.grey600_40
+    val grey600_50 = R.color.grey600_50
+    val grey600_60 = R.color.grey600_60
+    val grey600_68 = R.color.grey600_68
+    val grey600_80 = R.color.grey600_80
+
+    val grey500 = R.color.grey500
+    val grey500_40 = R.color.grey500_40
+
+    val grey400 = R.color.grey400
+    val grey400_40 = R.color.grey400_40
+    val grey400_50 = R.color.grey400_50
+
+    val grey200 = R.color.grey200
+    val grey200_40 = R.color.grey200_40
+    val grey200_70 = R.color.grey200_70
+    val grey200_75 = R.color.grey200_75
+
+    val grey100 = R.color.grey100
+
+    // Alpha
+    val dimmed = R.color.dimmed
+    val disabled = R.color.disabled
+
+    // Primary
+    val primary400 = R.color.primary400
+    val primary300 = R.color.primary300
+    val primary300_5 = R.color.primary300_5
+    val primary300_10 = R.color.primary300_10
+    val primary300_21 = R.color.primary300_21
+
+    val primary200 = R.color.primary200
+    val primary100 = R.color.primary100
+    val primary50 = R.color.primary50
+
+    // Accent
+    val red600 = R.color.red600
+    val red500 = R.color.red500
+    val red400 = R.color.red400
+    val red50 = R.color.red50
+
+    // Delivery
+    val orange600 = R.color.orange600
+    val orange600_80 = R.color.orange600_80
+    val orange50 = R.color.orange50
+
+    val blue800 = R.color.blue800
+    val blue500 = R.color.blue500
+    val blue50 = R.color.blue50
+
+    // Biz
+    val BizPrimary700 = R.color.BizPrimary700
+    val BizPrimary500 = R.color.BizPrimary500
+    val BizPrimary50 = R.color.BizPrimary50
+
+    // Old Style
+    val color_55DCFF = R.color.color_55DCFF
+    val color_E2F8FF = R.color.color_E2F8FF
+    val color_F2F2F4 = R.color.color_F2F2F4
+
+    val color_979798 = R.color.color_979798
+    val color_FF5B5B = R.color.color_FF5B5B
+    val color_FF5353 = R.color.color_FF5353
+    val color_EC2430 = R.color.color_EC2430
+    val color_D80017 = R.color.color_D80017
+    val color_E30613 = R.color.color_E30613
+    val color_E30613_7 = R.color.color_E30613_7
+    val color_EB0220 = R.color.color_EB0220
+    val color_DC0700 = R.color.color_DC0700
+    val color_C40025 = R.color.color_C40025
+    val color_F22100 = R.color.color_F22100
+    val color_E52222 = R.color.color_E52222
+    val color_F22100_40 = R.color.color_F22100_40
+    val color_F22100_99 = R.color.color_F22100_99
+    val color_FF404B_90 = R.color.color_FF404B_90
+
+    val color_FAE100 = R.color.color_FAE100
+    val color_FEE500 = R.color.color_FEE500
+    val color_FFDD00 = R.color.color_FFDD00
+    val color_FFD300 = R.color.color_FFD300
+    val color_F7B744 = R.color.color_F7B744
+    val color_FFA200 = R.color.color_FFA200
+    val color_FF8727 = R.color.color_FF8727
+    val color_FF7403 = R.color.color_FF7403
+    val color_FF7100 = R.color.color_FF7100
+    val color_FEE1CA = R.color.color_FEE1CA
+    val color_FFAA00 = R.color.color_FFAA00
+
+    val color_FF5722 = R.color.color_FF5722
+    val color_FF5823 = R.color.color_FF5823
+    val color_FF6F00 = R.color.color_FF6F00
+    val color_AF7F58 = R.color.color_AF7F58
+    val color_BE5F00 = R.color.color_BE5F00
+
+    val color_03C75A = R.color.color_03C75A
+    val color_6CCF10 = R.color.color_6CCF10
+    val color_75B843 = R.color.color_75B843
+    val color_70CD2B = R.color.color_70CD2B
+    val color_0CC500 = R.color.color_0CC500
+    val color_00A87D = R.color.color_00A87D
+    val color_00A23b = R.color.color_00A23b
+    val color_00AD9F = R.color.color_00AD9F
+    val color_008142 = R.color.color_008142
+    val color_8AD644 = R.color.color_8AD644
+
+    val color_DBF6FE = R.color.color_DBF6FE
+    val color_ABEDFF = R.color.color_ABEDFF
+    val color_D9F7FF = R.color.color_D9F7FF
+    val color_75E2FF = R.color.color_75E2FF
+    val color_EAF9FF = R.color.color_EAF9FF
+    val color_00AFFF = R.color.color_00AFFF
+    val color_0895DF = R.color.color_0895DF
+    val color_145b97 = R.color.color_145b97
+    val color_018EEC = R.color.color_018EEC
+    val color_00A1E9 = R.color.color_00A1E9
+    val color_2971F6 = R.color.color_2971F6
+    val color_007DFA = R.color.color_007DFA
+    val color_0D4D82 = R.color.color_0D4D82
+    val color_183290 = R.color.color_183290
+    val color_0046AF = R.color.color_0046AF
+    val color_0046AF_10 = R.color.color_0046AF_10
+    val color_0077D3 = R.color.color_0077D3
+
+    val color_4D4D4D_10 = R.color.color_4D4D4D_10
+
+    val E7E7E7_80 = R.color.E7E7E7_80
+
+    val color_CCCCCC = R.color.color_CCCCCC
+
+    val color_303233 = R.color.color_303233
+    val color_555555 = R.color.color_555555
+    val color_767676 = R.color.color_767676
+    val color_514646 = R.color.color_514646
+    val color_202020 = R.color.color_202020
+    val color_290000 = R.color.color_290000
+    val color_FEE7E6 = R.color.color_FEE7E6
+    val color_8D8D8D = R.color.color_8D8D8D
+    val color_A4A4A4 = R.color.color_A4A4A4
+    val color_888888 = R.color.color_888888
+    val color_DFF6FF = R.color.color_DFF6FF
+    val color_EBF9FF = R.color.color_EBF9FF
+    val color_F1FCFF = R.color.color_F1FCFF
+    val color_F8F8F8 = R.color.color_F8F8F8
+    val color_DCDCDC = R.color.color_DCDCDC
+    val color_E5E5E5 = R.color.color_E5E5E5
+
+    val color_AFB1B5 = R.color.color_AFB1B5
+    val color_F6F6F6 = R.color.color_F6F6F6
+
+}
